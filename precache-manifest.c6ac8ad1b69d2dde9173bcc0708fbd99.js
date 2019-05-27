aa6ca9015098b0473ed867938f39c1d3f6650e5e
Kacper Kuczewski <kucza06@wp.pl>
2019-05-27 19:27:32 +0200

    Updates

---
+++ precache-manifest.c6ac8ad1b69d2dde9173bcc0708fbd99.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3582145746b8cf94cfabb33eee9cdaa0",
+    "url": "/witcher-story-website/index.html"
+  },
+  {
+    "revision": "fbdd40fe5151f83e090d",
+    "url": "/witcher-story-website/static/css/main.53693439.chunk.css"
+  },
+  {
+    "revision": "c4abdf92eb34ffd1acd8",
+    "url": "/witcher-story-website/static/js/2.3c01b6c8.chunk.js"
+  },
+  {
+    "revision": "fbdd40fe5151f83e090d",
+    "url": "/witcher-story-website/static/js/main.a3ee41e1.chunk.js"
+  },
+  {
+    "revision": "f0aab4718daa26c75f86",
+    "url": "/witcher-story-website/static/js/runtime~main.562f6c93.js"
+  },
+  {
+    "revision": "1f1cae30afb16b69b0d710051930e913",
+    "url": "/witcher-story-website/static/media/dandelion2.1f1cae30.png"
+  },
+  {
+    "revision": "7410efc90fd364d8ac5faaa56f8dc274",
+    "url": "/witcher-story-website/static/media/dandelion2.7410efc9.jpg"
+  },
+  {
+    "revision": "75b9f03ce60bf1ad99be4a7b7e441384",
+    "url": "/witcher-story-website/static/media/dandelionIcon.75b9f03c.png"
+  },
+  {
+    "revision": "a31ebaf14be896adb5de170eb11730b1",
+    "url": "/witcher-story-website/static/media/kaermorhen.a31ebaf1.jpg"
+  },
+  {
+    "revision": "96413ba6b8c72b2aebaab2a7c58f5120",
+    "url": "/witcher-story-website/static/media/title.96413ba6.jpg"
+  },
+  {
+    "revision": "e67f8396b9a52bbe06ad542984064029",
+    "url": "/witcher-story-website/static/media/witcher.e67f8396.gif"
+  },
+  {
+    "revision": "7779bb5a1a6db6a52836305484b299a0",
+    "url": "/witcher-story-website/static/media/witcher1.7779bb5a.gif"
+  },
+  {
+    "revision": "a40522fd42623b044742c94b25fc1447",
+    "url": "/witcher-story-website/static/media/witcherInfo.a40522fd.jpg"
+  },
+  {
+    "revision": "55bcc08c7b6b6c05b1185b928bf1b056",
+    "url": "/witcher-story-website/static/media/witcherinfo1.55bcc08c.jpg"
+  }
+]);
